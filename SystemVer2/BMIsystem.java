688c4f8f27124f3323fe70161084a2fc2366250d
JALAL Likes Coffee <jalalfabrigas@gmail.com>
2026-05-20 14:17:27 +0000

    BMI PROGRESS

--- SystemVer2/BMIsystem.java
+++ SystemVer2/BMIsystem.java
@@ -1,5 +1,18 @@
 package SystemVer2;
 import java.util.*;
+
 public class BMIsystem {
+    public void bmi (String args[]){
+        Scanner sc = new Scanner (System.in);
+
+        System.out.println("===WELCOME TO BMI SYSTEM===");
+        System.out.print("Enter your height: ");
+        double height = sc.nextDouble();
+        System.out.print("Enter your weight: ");
+        double weight = sc.nextDouble();
+        
+        double bmiResult = weight / (height * height);
+        System.out.println("RESULT: " + bmiResult);
+    }
     
 }

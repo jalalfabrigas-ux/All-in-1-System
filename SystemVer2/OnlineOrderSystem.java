3915fc410893c09a26f78aa7abc21833fb02bbc3
JALAL Likes Coffee <jalalfabrigas@gmail.com>
2026-05-23 12:46:58 +0000

    OOS PROGRESS

--- SystemVer2/OnlineOrderSystem.java
+++ SystemVer2/OnlineOrderSystem.java
@@ -37,4 +37,34 @@ public class MenuItem{
             return getPrice() * quantity;
         }
     }
-}
+    public class DrinkItem extends MenuItem{
+        private int size;
+        public DrinkItem(String name, double price, int size){
+            super(name,price);
+            this.size=size;
+        }   public int getterSize(){
+            return size;
+        }   public void setterSize(int size){
+            this.size=size;
+        }
+    }
+    public class Main{
+        public static void main(String args[]){
+            Scanner sc=new Scanner(System.in);
+
+            System.out.print("\n HOW MANY ORDERS? ");
+            int n = sc.nextInt();
+
+            MenuItem[] order = new MenuItem[n];
+
+            for(int i=0;i<n;i++){
+                System.out.print("\n TYPE OF ORDERS?");
+                System.out.print("\n 1. FOODS | 2. DRINKS");
+                int type=sc.nextInt();
+                if(type == 1){
+                    System.out.print("\n===FOOD MENU===");
+                }
+                
+            }
+        }
+    }
--- SystemVer2/SystemTwo.java
+++ SystemVer2/SystemTwo.java
@@ -3,6 +3,7 @@ import java.util.*;
 public class SystemTwo {
     public static void main(String args[]){
         Scanner sc=new Scanner(System.in);
+        int choice =sc.nextInt();
         
         System.out.println("WELCOME TO  VERSION 2 OF ALL in One System!");
         System.out.print("\n1. Age Calculator System");

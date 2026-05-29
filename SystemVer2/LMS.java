6e7417c8da5cb5660b7e3a64d6abab2a4c39aa0c
JALAL Likes Coffee <jalalfabrigas@gmail.com>
2026-05-29 13:29:27 +0000

    library management system

--- SystemVer2/LMS.java
+++ SystemVer2/LMS.java
@@ -1,6 +1,7 @@
 package SystemVer2;
 import java.util.*;
 
-public class LMS{
+public class LMS{   
+//Library Management System
 public static void main(String args[]){
 Scanner sc=new Scanner(Systm.in);

8c4731a88026327d8dc6a45f2db915b3f2de1eb3
JALAL Likes Coffee <jalalfabrigas@gmail.com>
2026-06-01 14:03:15 +0000

    changes

--- SystemVer2/LMS.java
+++ SystemVer2/LMS.java
@@ -4,4 +4,8 @@ import java.util.*;
 public class LMS{   
 //Library Management System
 public static void main(String args[]){
-Scanner sc=new Scanner(Systm.in);
+    Scanner sc=new Scanner(System.in);
+
+    System
+}
+}
